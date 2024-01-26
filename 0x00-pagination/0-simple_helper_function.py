127673b93990594b1e93df79cd81818dc19e3a13
kelechi <paulkelechi11@gmail.com>
2024-01-26 11:03:26 +0000

    Add: New script that returns a tuple of start and end dates of two int inputs

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" defines function index_range """
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculates the start and end index for a specific
+    page and page size in a list.
+    """
+    start_index = p
+
+    end_index =
+
+    return (start_index, end_index)
