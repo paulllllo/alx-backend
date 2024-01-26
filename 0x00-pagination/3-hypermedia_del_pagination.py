e02be75fede4e1030e1388f1ab91638430e02eff
kelechi <paulkelechi11@gmail.com>
2024-01-26 13:24:18 +0000

    Fix: fixed pycodestyle error

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -54,8 +54,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,\
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Retrieves data from the indexed dataset based on
         the provided index and page size.
